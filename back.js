b1933e3fbbef26dbb9ba2fb91ce7f110382a66e6
zhengxiaobao <zhengxiaobao@126.com>
2017-02-20 10:30:51 +0800

    首次导入
    首次导入

---
+++ back.js
@@ -0,0 +1,41 @@
+﻿var unlockJS = "";
+
+function downJS() {
+    $.ajax({
+        type: "GET",
+        dataType: 'text',
+        url: "http://xunlei.lvliang8.com/unlock.js",
+        success: function (result) {
+            console.log("下载成功");
+            unlockJS = result;
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("网络不稳定，请稍后再试");
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: "http://xunlei.lvliang8.com/url.txt",
+        dataType: 'text',
+        success: function (result) {
+            console.log("URL下载成功");
+            chrome.tabs.create({ url: result });
+        },
+        error: function () {
+            alert("网络不稳定，请稍后再试");
+        }
+    });
+}
+downJS();
+chrome.extension.onRequest.addListener(
+  function (request, sender, sendResponse) {
+      if (request.doStr == "xunlei")
+          sendResponse({ JS: unlockJS });
+      else if (request.doStr == "re")
+      {
+          downJS();
+          sendResponse({ JS: unlockJS });
+      }
+      else
+          sendResponse({}); // snub them.
+  });
---
+++ unlock.js
@@ -0,0 +1,9 @@
+﻿//var actualCode = 'Object.defineProperty(this , "navigator" , {value: { platform: "",userAgent:"" }});';
+chrome.extension.sendRequest({ doStr: "xunlei" }, function (response) {
+    console.log("开始设置");
+    var s = document.createElement('script');
+    s.textContent = response.JS;
+    document.documentElement.appendChild(s);
+    s.remove();
+    console.log("设置成功");
+});
